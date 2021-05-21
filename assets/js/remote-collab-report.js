7cfaabbc0a2385d9e5087ce447da8ffe84533a3f
suukii <suki@vibe.us>
2021-05-21 13:45:50 +0800

    Refine toc

--- assets/js/remote-collab-report.js
+++ assets/js/remote-collab-report.js
@@ -11,7 +11,7 @@ const updateToc = (function () {
     tocItems.forEach(tocItem => {
       const isActive = activeId === tocItem.dataset.id;
       const op = isActive ? 'add' : 'remove';
-      tocItem.classList[op]('is-active');      
+      tocItem.classList[op]('is-active');
     });
     subItemsEls.forEach(el => {
       const hasActiveChild = el.querySelector('.subitem.is-active');
@@ -23,21 +23,78 @@ const updateToc = (function () {
   }
 })()
 
+const GLOBALS_ = {
+  hashchanged: false,
+};
+
+getLastActiveToc();
 observeHeaders(updateToc);
 
+window.addEventListener('hashchange', () => {
+  GLOBALS_.hashchanged = true;
+  const id = location.hash.slice(1);
+  if (id) {
+    updateToc(id);
+    saveLastActiveToc(id);
+  }
+})
+
 function observeHeaders(notify) {
-  const headers = document.querySelectorAll('h2, h3')
+  const headers = [...document.querySelectorAll('.main-content h2, .main-content h3')]
   const options = {
-    rootMargin: `0% 0% -80% 0%`
+    rootMargin: `0% 0% -80% 0%`,
+    threshold: 0.8
   }
   const observer = new IntersectionObserver(entries => {
+    // If changes were caused by clicking the TOC item, leave them.
+    if (GLOBALS_.hashchanged) {
+      GLOBALS_.hashchanged = false;
+      return;
+    }
+
+    let activeItem = null;
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        notify(entry.target.id);
+        activeItem = entry.target;
+      } else {
+        if (!activeItem) {
+          // when a header slides down from the observing area,
+          // highlight the header above it
+          if (entry.boundingClientRect.top < entry.rootBounds.bottom
+            && entry.boundingClientRect.bottom > entry.rootBounds.bottom) {
+            const index = headers.findIndex(el => el === entry.target);
+            if (index > 0) {
+              activeItem = headers[index - 1];
+            }
+          } 
+          // when a header slides up from the observing area,
+          // highlight the header itself
+          else if (entry.boundingClientRect.top < entry.rootBounds.top
+            && entry.boundingClientRect.bottom > entry.rootBounds.top) {
+              activeItem = entry.target;
+            }
+        };
       }
     })
+    if (activeItem) {
+      notify(activeItem.id);
+      saveLastActiveToc(activeItem.id);
+    }
   }, options)
   headers.forEach(item => observer.observe(item));
+}
+
+function saveLastActiveToc(id) {
+  sessionStorage.setItem('lastActiveTocId', id);
+}
+
+function getLastActiveToc() {
+  const id = sessionStorage.getItem('lastActiveTocId')
+  id && updateToc(id);
+}
+
+function convertRemToPixels(rem) {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
 const navbar = document.querySelector('.navbar');
